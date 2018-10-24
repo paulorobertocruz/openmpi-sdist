72f2b2acbf20fa64bb6fb50bd58718707d54a794
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-10-24 17:02:41 -0300

    add print_array, add pixel_kernel_filter, image_kernel_filter

--- sdist.c
+++ sdist.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "sdist.h"
 
 // utils
@@ -15,8 +17,30 @@ void print_matrix(int **m, int r, int c)
     }
 }
 
+void print_array(int * array, int size)
+{
+    for ( int i = 0; i < size; i ++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
 // vetores
 //---------------------------------------------------------
+
+int * get_array(int size)
+{
+    int *array;
+
+    array = (int *) malloc(size * sizeof(int));
+    for ( int i = 0; i < size; i++)
+    {
+        array[i] = 0;
+    }
+
+    return array;
+}
+
 int ** get_2d_matrix(int a, int b)
 {
     int **m;
@@ -81,7 +105,7 @@ int ** multply_matrix(int **matriz_a, int a_rows, int a_cols, int **matriz_b, in
 
 // processamento de imagem 
 //---------------------------------------------------------
-int image_kernel_filter(int *image, int *kernel, int length)
+int pixel_kernel_filter(int *pixels, int *kernel, int length, int coeficient)
 {
     int soma;
     soma = 0;
@@ -93,8 +117,67 @@ int image_kernel_filter(int *image, int *kernel, int length)
     
     for (int i = 0; i < length; i++)
     {
-        soma+= image[i] * kernel[i];
+        soma += pixels[i] * kernel[i];
     }
 
-    return soma/length;
+    if ( soma == 0)
+    {
+        return 0;
+    }
+    return soma/coeficient;
+}
+
+
+int ** image_kernel_filter(int **image, int image_width, int image_height, int *kernel, int kernel_size)
+{
+    // o tamanho do kernel deve ser igual a kernel_size * kernel_size
+    // kernel_size deve ser um numero impar >= 3
+
+    int ** imagem_resposta;
+    int *pixels;
+
+    int coeficient, contador_pixels;
+
+    pixels = (int *)malloc(kernel_size * kernel_size * sizeof(int));
+    
+    //inicializa matriz resposta
+    imagem_resposta = get_2d_matrix(image_width, image_height);
+    
+    for (int i = 0; i < image_width; i++)
+    {
+        for ( int j = 0; j < image_height; j++)
+        {
+            
+            coeficient = 0;
+            contador_pixels = 0;
+            //pega os valores para aplicar o filtro
+            for (int k = i - 1; k < kernel_size; k++)
+            {
+                for ( int w = j-1; w < kernel_size; w++)
+                {
+
+                    //verifica se o pixel existe
+                    if( k >= 0 && w >= 0 && k < image_width && w < image_height)
+                    {
+                        coeficient ++;
+                        pixels[contador_pixels] = image[k][w];
+
+                    }
+                    else
+                    {
+                        pixels[contador_pixels] = 0;
+                    }
+                    contador_pixels++;
+                }
+            }
+
+            //chama filtro no pixel
+            int resposta = pixel_kernel_filter(pixels, kernel, kernel_size * kernel_size, coeficient);
+            imagem_resposta[i][j] = resposta;
+
+        }
+    }
+
+    return imagem_resposta;
+
 }
--- sdist.h
+++ sdist.h
@@ -2,15 +2,18 @@
 #define _sdist_h
 
 //utils
+void print_array(int * array, int size);
 void print_matrix(int **m, int r, int c);
 
 //vetores
+int * get_array(int size);
 int ** get_2d_matrix(int a, int b);
 int ** get_transpose_2d_matrix(int **m, int r, int c);
 int dot_product(int *a, int *b, int size);
 int ** multply_matrix(int **A, int a_rows, int a_cols, int **B, int b_rows, int b_cols);
 
 //processamento de imagem
-int image_kernel_filter(int *image, int *kernel, int length);
+int pixel_kernel_filter(int *pixels, int *kernel, int length, int coeficient);
+int ** image_kernel_filter(int **image, int image_width, int image_height, int *kernel, int kernel_size);
 
 #endif
